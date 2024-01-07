86888d308c9ce3dff2813eedc757980ed9baca71
Spencer Hum <spencerhum04@gmail.com>
2024-01-07 15:40:12 -0500

    add

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,7 @@
 import styles, { layout } from '../style';
-import { languages } from '../constants';
+import { projects } from '../constants';
 
-const LanguageCard = ({ icon }) => (
+const ProjectCard = ({ icon }) => (
   <div className="flex flex-col items-center p-6 mb-6 mt-2 mr-5 bg-zinc-700 hover:bg-blue-300">
     <div className={`w-[499px] h-[250px] ${styles.flexCenter}`}>
       <img src={icon} className="w-[50%] h-[50%] object-contain" />
@@ -15,10 +15,10 @@ const Projects = () => {
       <div className={layout.sectionInfo}>
         <h2 className={`${styles.heading2} underline decoration-cyan-300`}>Projects</h2>
 
-        {/* Languages Cards */}
+        {/* Projects Cards */}
         <div className="flex flex-wrap justify-start mt-1">
-          {languages.map((language,index) => (
-            <LanguageCard key={language.id} {...language} index={index} />
+          {projects.map((project,index) => (
+            <ProjectCard key={project.id} {...project} index={index} />
           ))}
         </div>
 
