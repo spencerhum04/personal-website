ee53922e87e639daed02dcfaf2cab17a259a1bb8
Spencer Hum <spencerhum04@gmail.com>
2024-01-31 13:22:30 -0500

    hero section and tweakings

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,19 +1,58 @@
+import React, { useEffect, useRef } from 'react';
 import styles from '../style';
-import { githublogo, linklogo, rightarrowicon } from '../assets';
-import { Link } from 'react-router-dom';
 
-const Hero = () => (
-  <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
-    <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6 ml-12`}>
-      <div className="flex flex-row justify-between items-center w-full mt-3">
-        <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]">
-          Hello! I'm<br className="sm:block hidden" /> {" "}
-          Spencer Hum<br className="sm:block hidden" /> {" "}
-          <span className="text-gradient">IM DESPERATE NEED JOB PLEASE I NEED A JOB</span>
-        </h1>
+const Hero = () => {
+  const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const h1Ref = useRef(null);
+  let interval = null;
+
+  const startTextAnimation = () => {
+    let iteration = 0;
+
+    clearInterval(interval);
+
+    interval = setInterval(() => {
+      h1Ref.current.innerText = h1Ref.current.dataset.value
+        .split("")
+        .map((letter, index) => {
+          if (index < iteration) {
+            return h1Ref.current.dataset.value[index];
+          }
+
+          return letters[Math.floor(Math.random() * 26)];
+        })
+        .join("");
+
+      if (iteration >= h1Ref.current.dataset.value.length) {
+        clearInterval(interval);
+      }
+
+      iteration += 1 / 3;
+    }, 100);
+  };
+
+  useEffect(() => {
+    const h1Element = h1Ref.current;
+
+    if (h1Element) {
+      startTextAnimation(); // Start animation automatically
+    }
+  }, []); // Removed [interval] from the dependencies array
+
+  return (
+    <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
+      <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6 ml-12`}>
+        <div className="flex flex-row justify-between items-center w-full mt-3">
+          <h1
+            ref={h1Ref}
+            className="flex-1 font-mono font-black ss:text-[100px] text-[52px] text-white ss:leading-[100px] leading-[75px] flex justify-center items-center h-screen"
+            data-value="SPENCER HUM"
+          >
+          </h1>
+        </div>
       </div>
-    </div>
-  </section>
-)
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
