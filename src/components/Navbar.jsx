9a5815c837f71f977cb48eb6022f400cd3d6a314
spencerhum04 <spencerhum04@gmail.com>
2024-02-26 09:29:46 -0500

    ui changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,10 +44,10 @@ const Navbar = () => {
                 </div> */}
                 <div className="flex flex-1 justify-end items-center">
                     <img src={toggle ? close : menu} alt="menu" className="w-[28px] h-[28px] object-contain cursor-pointer" onClick={() => setToggle((prev) => !prev)} />
-                    <div className={`${toggle ? 'flex' : 'hidden'} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-[16px] sidebar`}>
+                    <div className={`${toggle ? 'flex' : 'hidden'} p-6 absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-[16px] sidebar`}>
                         <ul className="list-none flex flex-col justify-end items-center flex-1">
                             {navLinks.map((nav, index) => (
-                                <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.length - 1 ? 'mr-0' : 'mb-4'} text-white`}>
+                                <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[18px] ${index === navLinks.length - 1 ? 'mr-0' : 'mb-14'} text-white`}>
                                     <a href={`#${nav.id}`}>
                                         {nav.title}
                                     </a>
